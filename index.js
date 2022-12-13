79c5ff284e0a7d2dc90a9bac7f9414f66d6d52ab
Yash Rami <yashrami77@gmail.com>
2022-12-13 14:28:54 +0530

    Add On: Videos page

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("script is added")
+document.querySelector('.cross').style.display="none";
+document.querySelector('.hamburger').addEventListener('click',()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+    if( document.querySelector('.sidebar').classList.contains('sidebargo')){
+        document.querySelector('.menubar').style.display ="inline"
+        document.querySelector('.cross').style.display ="none"
+    }
+    else{
+        document.querySelector('.menubar').style.display ="none"
+        setTimeout(() => {
+            document.querySelector('.cross').style.display ="inline"    
+        }, 400);
+        
+    }
+})
